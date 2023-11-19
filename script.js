508bbf0e00e25b601d0b56e96fcd9e19b2738613
saadansari923 <135580982+saadansari923@users.noreply.github.com>
2023-11-19 22:04:35 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,171 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js";
+
+import {
+  getFirestore,
+  getDocs,
+  doc,
+  collection,
+  addDoc,
+  deleteDoc,
+} from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDXWP3H7Lpa-Xj-ORLqp8KSSF0h5HQPOpE",
+  authDomain: "blogs-ec2b1.firebaseapp.com",
+  databaseURL: "https://blogs-ec2b1-default-rtdb.firebaseio.com",
+  projectId: "blogs-ec2b1",
+  storageBucket: "blogs-ec2b1.appspot.com",
+  messagingSenderId: "276245964646",
+  appId: "1:276245964646:web:cbee6364b1f9b5f9654aa7",
+  measurementId: "G-0RQW9X161N",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+//initialize
+const db = getFirestore(app);
+const todosCollectionRef = collection(db, "todos");
+
+const registerForm = document.getElementById("register-form");
+const loginForm = document.getElementById("login-form");
+const loader = document.getElementById("loader-div");
+const userDiv = document.getElementById("user-info");
+const auhDiv = document.getElementById("auth");
+const logout = document.getElementById("logout");
+const userEmail = document.getElementById("user-email");
+const addInfo = document.getElementById("addInfo");
+const todoInput = document.getElementById("todo_input");
+const todosContainer = document.getElementById("todos_container");
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/auth.user
+    const uid = user.uid;
+    console.log("user is logged in");
+
+    loader.style.display = "none";
+    auhDiv.style.display = "none";
+    userDiv.style.display = "block";
+    userEmail.innerText = `User email is ${user.email} and User uid is ${uid}`;
+    getTodos();
+    // ...
+  } else {
+    // User is signed out
+    loader.style.display = "none";
+    auhDiv.style.display = "block";
+    userDiv.style.display = "none";
+
+    // ...
+  }
+});
+
+registerForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+  const email = e.target[0].value;
+  const password = e.target[1].value;
+
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert("User is not register because of " + errorMessage);
+    });
+});
+
+loginForm?.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log(e);
+  const email = e.target[0].value;
+  const password = e.target[1].value;
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed up
+      const user = userCredential.user;
+      window.location.href = "/";
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert("User is not logged in because of " + errorMessage);
+    });
+});
+
+logout.addEventListener("click", () => {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+      console.log("signedout");
+    })
+    .catch((error) => {
+      // An error happened.
+      console.log("signedout", error);
+    });
+});
+
+//DB Section--->
+
+addInfo.addEventListener("click", async () => {
+  if (!todoInput.value) return alert("Please add todo");
+  try {
+    const docAdded = await addDoc(todosCollectionRef, {
+      todo: todoInput.value,
+    });
+    todoInput.value = "";
+    getTodos();
+    console.log("Document written with ID: ", docAdded);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+});
+
+async function getTodos() {
+  todosContainer.innerHTML = null;
+  const querySnapshot = await getDocs(todosCollectionRef);
+  querySnapshot.forEach((todoDoc) => {
+    const todoObj = todoDoc.data();
+    const div = document.createElement("div");
+    div.className = "todo-div";
+    const span = document.createElement("span");
+    span.innerText = todoObj.todo;
+    const button = document.createElement("button");
+    button.innerText = "Delete";
+    button.id = todoDoc.id;
+
+    button.addEventListener("click", async function () {
+      console.log(this);
+
+      const docRef = doc(db, "todos", this.id);
+      console.log(docRef);
+      await deleteDoc(docRef);
+      getTodos();
+    });
+
+    div.appendChild(span);
+    div.appendChild(button);
+
+    todosContainer.appendChild(div);
+  });
+}
+
+//1. getFirestore  // to initialize firestore,
+//2. getDocs,  // to get all the documents in the particular collection
+//3. doc,   // to create the reference of the single document
+//4. collection // to create the reference of the single collection,
+//5. addDoc, // to add the document in the collection , it takes two arguments 1.collection reference 2.Document
+//6. deleteDoc // to delete the single document , it takes doc reference as an argument
